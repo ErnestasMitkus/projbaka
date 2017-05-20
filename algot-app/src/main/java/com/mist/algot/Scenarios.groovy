e7b6fd7f7e4b495a4eb01a9b3b453c30923bef4f
Ernestas Mitkus <ernestas@inventi.lt>
2017-05-20 13:19:42 +0300

    add trees

--- algot-app/src/main/java/com/mist/algot/Scenarios.groovy
+++ algot-app/src/main/java/com/mist/algot/Scenarios.groovy
@@ -70,11 +70,23 @@ class Scenarios {
 
     static List<Entity> trees() {
         float dist = 100
+        float dist1 = 150
+        float dist2 = 200
+        float dist3 = 200
+        float dist4 = 50
         float toRad = Math.PI / 180f
         def aroundTrees = (0..29).collect {
             def radians = (it * (360f / 30f)) * toRad
+            def radians1 = (it * (360f / 30f) + 6f) * toRad
+            def radians2 = (it * (360f / 30f) + 3f) * toRad
+            def radians3 = (it * (360f / 30f) - 3f) * toRad
+            def radians4 = (it * (360f / 30f)) * toRad
 
-            [new Vector3f(dist * Math.sin(radians) as float, 0, dist * Math.cos(radians) as float)]
+            [new Vector3f(dist * Math.sin(radians) as float, 0, dist * Math.cos(radians) as float),
+             new Vector3f(dist1 * Math.sin(radians1) as float, -6, dist1 * Math.cos(radians1) as float),
+             new Vector3f(dist2 * Math.sin(radians2) as float, 40, dist2 * Math.cos(radians2) as float),
+             new Vector3f(dist3 * Math.sin(radians3) as float, 40, dist3 * Math.cos(radians3) as float)
+            ] + (it % 2 == 0 ? [new Vector3f(dist4 * Math.sin(radians4) as float, -10, dist4 * Math.cos(radians4) as float)] : [])
         }.flatten() as List<Vector3f>
 
         aroundTrees.collect {
@@ -86,6 +98,7 @@ class Scenarios {
         Map<String, Closure<List<Entity>>> scenarios = [
             cube: Scenarios.&cube,
             dragons: Scenarios.&dragons,
+            trees: Scenarios.&trees,
         ]
         scenarios[name].call()
     }
